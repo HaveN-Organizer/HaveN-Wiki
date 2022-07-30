88bdb4fbf0f9c0d17306e19238bff41e8a6f4cf2
Gucci Fox <pickle56309@gmail.com>
2022-07-30 12:23:02 -0400

    Enable broken link warn

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,7 +10,7 @@ const config = {
   tagline: 'The official HaveN Studios Wiki',
   url: 'https://wikihaven.vercel.app',
   baseUrl: '/',
-  onBrokenLinks: 'ignore',
+  onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
 
